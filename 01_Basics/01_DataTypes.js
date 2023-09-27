8567dbe464b85cab6bb53ea1aff757ad95b099e1
imranahmedawan <imranmcs@gmail.com>
2023-09-27 21:11:37 +0500

    DataTypes

--- 01_Basics/01_DataTypes.js
+++ 01_Basics/01_DataTypes.js
@@ -1,3 +1,19 @@
 "use strict" // treat all JS code as newer version
 console.log(3+3) 
 console.log("Imran Ahmed Awan")
+var age =100
+//number => 2 to 52
+//bigint
+//string => ""
+//boolean => true/false
+//null=> standalone value
+//undefined
+//symbol
+
+
+//object
+
+console.log(typeof "Imran");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
