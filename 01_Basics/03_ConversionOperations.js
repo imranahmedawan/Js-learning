d1491ede47d8ccce4b5a72e457dadf02d2b1163e
imranahmedawan <imranmcs@gmail.com>
2023-09-28 09:38:18 +0500

    04_Comparisons

--- 01_Basics/03_ConversionOperations.js
+++ 01_Basics/03_ConversionOperations.js
@@ -1,12 +1,12 @@
 let score = "33A"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
 
-console.log(valueInNumber);
+//console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" -> NAN
@@ -14,3 +14,32 @@ console.log(valueInNumber);
 // "Imran" => NAN
 // Undefined => NAN (not a number)
 // true => 1
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(8%3);
+
+let str1 ="Hello";
+let str2 = " Imran";
+let str3 = str1 + str2;
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let x = 3;
+let y = x++;
+console.log("x:" + x, "y:" + y);
+
+let a = 3;
+let b = ++a;
+console.log("a:" + a, "b:" + b);
